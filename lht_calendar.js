31a03231244fc54f792cd81c7152039af1e2406b
Gabriel Torres <gtorres043@west-mec.org>
2019-02-19 18:22:31 -0700

    added funtions to javascript file

--- lht_calendar.js
+++ lht_calendar.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Tutorial Case
 
-   Author: 
-   Date:  
+   Author: Gabriel Torres
+   Date:  2/19/19
 
    Filename:   lht_calendar.js  
 
@@ -29,3 +29,22 @@
       Writes the daily rows in the calendar table, highlighting calDate
 	
 */
+
+//Set the date displayed in the caleneder
+var thisDay = new Date("August 24, 2018");
+
+//Write the calender to the element with the id "calender"
+document.getElementById("calender").innerHTML = createCalender(thisDay);
+
+//create the function to generate the calender table 
+
+function createCalender(calDate) {
+      var calenderHTML = "<table id='calender_table'>";
+      calenderHTML += "</table>";
+      return calenderHTML;
+}
+
+//function to write the calender caption
+function calCaption(calDate)(
+      //monthName array contains the list of month names
+      var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
